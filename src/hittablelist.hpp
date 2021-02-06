cdc925a4e7e19c084f26f383ec11f5e661394ad2
Marcythm <marcythm@gmail.com>
2021-02-06 19:50:18 +0800

    6.5. A List of Hittable Objects

---
+++ src/hittablelist.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "config.hpp"
+#include "hittable.hpp"
+
+class HittableList: Hittable {
+    Vec<ptr<Hittable>> objects;
+
+public:
+    HittableList() = default;
+
+    auto clear() -> void { objects.clear(); }
+    auto push(const ptr<Hittable> obj) -> void { objects.push_back(obj); }
+
+    auto hit(const Ray &ray, const f64 t_min, const f64 t_max) const -> std::pair<bool, HitRecord> {
+        std::pair<bool, HitRecord> sol;
+        f64 closest = t_max;
+
+        for (const auto &obj: objects)
+            if (const std::pair<bool, HitRecord> subsol = obj->hit(ray, t_min, closest); subsol.first)
+                closest = subsol.second.t, sol = subsol;
+
+        return sol;
+    }
+};
