edaba43b114e88f397a935ba6fdd670f359f6deb
Marcythm <marcythm@gmail.com>
2021-02-06 20:20:21 +0800

    7.1. Some Random Number Utilities

--- src/lib.hpp
+++ src/lib.hpp
@@ -7,6 +7,15 @@ constexpr f64 pi = 3.1415926535897932385;
 
 inline auto deg2rad(const f64 deg) -> f64 { return deg * pi / 180.0; }
 
+inline auto random_f64() -> f64 {
+    static std::uniform_real_distribution<f64> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+inline auto random_f64(const f64 min, const f64 max) -> f64 {
+    return min + (max - min) * random_f64();
+}
+
 #include "p3d.hpp"
 #include "vec3.hpp"
 #include "rgb.hpp"
