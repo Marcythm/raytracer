1ac0a7eaa2edbe410b3a0f01512d15067a084244
Marcythm <marcythm@gmail.com>
2021-02-07 19:35:24 +0800

    11.2. Positioning and Orienting the Camera

--- src/camera.hpp
+++ src/camera.hpp
@@ -12,18 +12,27 @@ class Camera {
     p3d lower_left_corner;
 
 public:
-    constexpr Camera(const f64 vertical_field_of_view, const f64 aspect_ratio): origin(0, 0, 0) {
+    constexpr Camera(
+        const p3d &lookfrom, const Vec3 &lookdir, const Vec3 &viewup,
+        const f64 vertical_field_of_view, const f64 aspect_ratio
+    ): origin(lookfrom) {
         const f64 theta = deg2rad(vertical_field_of_view);
         const f64 h = std::tan(theta / 2);
         const f64 viewport_height = 2 * h;
         const f64 viewport_width = aspect_ratio * viewport_height;
 
-        const f64 focal_length = 1.0;
+        const Vec3 w = (-lookdir).unit();
+        const Vec3 u = cross(viewup, w).unit();
+        const Vec3 v = cross(w, u);
 
-        horizontal = Vec3(viewport_width, 0.0, 0.0);
-        vertical = Vec3(0.0, viewport_height, 0.0);
-        lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, focal_length);
+        horizontal = viewport_width * u;
+        vertical = viewport_height * v;
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
     }
+    constexpr Camera(
+        const p3d &lookfrom, const p3d &lookat, const Vec3 &viewup,
+        const f64 vertical_field_of_view, const f64 aspect_ratio
+    ): Camera(lookfrom, lookat - lookfrom, viewup, vertical_field_of_view, aspect_ratio) {}
 
     constexpr auto get_ray(const f64 u, const f64 v) const -> Ray {
         return Ray(origin, lower_left_corner + u * horizontal + v * vertical);
--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,21 @@
 
 auto main() -> i32 {
 	// World
-	const f64 R = std::cos(constants::pi / 4);
-	auto material_left  = std::make_shared<Lambertian>(RGB(0, 0, 1));
-	auto material_right = std::make_shared<Lambertian>(RGB(1, 0, 0));
+	const auto material_ground = std::make_shared<Lambertian>(RGB(0.8, 0.8, 0.0));
+	const auto material_center = std::make_shared<Lambertian>(RGB(0.1, 0.2, 0.5));
+	const auto material_left   = std::make_shared<Dielectric>(1.5);
+	const auto material_right  = std::make_shared<Metal>(RGB(0.8, 0.6, 0.2), 0.0);
 
 	HittableList world;
-	world.push(std::make_shared<Sphere>(p3d(-R, 0, -1), R, material_left));
-	world.push(std::make_shared<Sphere>(p3d( R, 0, -1), R, material_right));
+	world.push(std::make_shared<Sphere>(p3d( 0.0, -100.5, -1.0), 100.0, material_ground));
+	world.push(std::make_shared<Sphere>(p3d( 0.0,    0.0, -1.0),   0.5, material_center));
+	world.push(std::make_shared<Sphere>(p3d(-1.0,    0.0, -1.0),   0.5, material_left));
+	world.push(std::make_shared<Sphere>(p3d(-1.0,    0.0, -1.0), -0.45, material_left));
+	world.push(std::make_shared<Sphere>(p3d( 1.0,    0.0, -1.0),   0.5, material_right));
 
 	// Camera
-	const Camera camera(90, constants::aspect_ratio);
+	const Camera camera(p3d(-2, 2, 1), p3d(0, 0, -1), Vec3(0, 1, 0), 20, constants::aspect_ratio);
+	// const Camera camera(p3d(-2, 2, 1), p3d(0, 0, -1), Vec3(0, 1, 0), 90, constants::aspect_ratio);
 
 	// Render
 	std::cout.tie(0);
