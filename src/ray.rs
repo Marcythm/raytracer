0c6d35db23c6bc51213f5cde5c5a7a1716c77f0d
Marcythm <marcythm@gmail.com>
2021-02-08 22:59:14 +0800

    6.1. Shading with Surface Normals

--- src/ray.rs
+++ src/ray.rs
@@ -26,20 +26,26 @@ impl Ray {
     }
 
     pub fn color(&self) -> RGB {
-        if self.hit_sphere(P3d::new(0.0, 0.0, -1.0), 0.5) {
-            RGB::new(1.0, 0.0, 0.0)
+        let t = self.hit_sphere(P3d::new(0.0, 0.0, -1.0), 0.5);
+        if t > 0.0 {
+            let normal = (self.at(t) - P3d::new(0.0, 0.0, -1.0)).unit();
+            0.5 * RGB::new(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0)
         } else {
             let t = 0.5 * (self.direction.unit().y() + 1.0);
             (1.0 - t) * RGB::new(1.0, 1.0, 1.0) + t * RGB::new(0.5, 0.7, 1.0)
         }
     }
 
-    pub fn hit_sphere(&self, center: P3d, radius: f64) -> bool {
+    pub fn hit_sphere(&self, center: P3d, radius: f64) -> f64 {
         let oc = self.origin - center;
         let a = Vec3::dot(&self.direction, &self.direction);
         let b = 2.0 * Vec3::dot(&oc, &self.direction);
         let c = oc.length2() - radius * radius;
         let discriminant = b * b - 4.0 * a * c;
-        discriminant > 0.0
+        if discriminant < 0.0 {
+            -1.0
+        } else {
+            (-b - discriminant.sqrt()) / (2.0 * a)
+        }
     }
 }
