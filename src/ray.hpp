c8a8968423422810971d3877f799565f4b2d10ea
Marcythm <marcythm@gmail.com>
2021-02-09 02:16:01 +0800

    2.1. Introduction of SpaceTime Ray Tracing

--- src/ray.hpp
+++ src/ray.hpp
@@ -9,15 +9,17 @@ class Ray {
 
     p3d _origin;
     Vec3 _direction;
+    f64 _time;
 
 public:
-    constexpr Ray() { }
-    constexpr Ray(const Self &other): _origin(other._origin), _direction(other._direction) {}
-    constexpr Ray(const p3d &ori, const Vec3 &dir): _origin(ori), _direction(dir) {}
-    constexpr Ray(const p3d &src, const p3d &dst): _origin(src), _direction(dst - src) {}
+    constexpr Ray(): _time(0.0) { }
+    constexpr Ray(const Self &other): _origin(other._origin), _direction(other._direction), _time(other._time) {}
+    constexpr Ray(const p3d &ori, const Vec3 &dir, const f64 t = 0.0): _origin(ori), _direction(dir), _time(t) {}
+    constexpr Ray(const p3d &src, const p3d &dst, const f64 t = 0.0): _origin(src), _direction(dst - src), _time(t) {}
 
     constexpr auto origin() const -> p3d { return _origin; }
     constexpr auto direction() const -> Vec3 { return _direction; }
+    constexpr auto time() const -> f64 { return _time; }
 
     constexpr auto at(const f64 t) const -> p3d { return _origin + t * _direction; }
 
