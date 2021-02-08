bd76293f60ec451bd8f68e97c3a8f172ebe480f9
Marcythm <marcythm@gmail.com>
2021-02-08 23:06:35 +0800

    10.3. Total Internal Reflection

--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -17,8 +17,14 @@ impl Dielectric {
 impl Material for Dielectric {
     fn scatter(&self, ray: &Ray, rec: &HitRecord, _: &mut SmallRng) -> Option<(Ray, RGB)> {
         let unit_direction = ray.direction().unit();
+        let cos_theta = Vec3::dot(-unit_direction, rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
         let etai_over_etat = if rec.front_face { 1.0 / self.refractive_index } else { self.refractive_index };
 
-        Some((Ray::new(rec.p, unit_direction.refract_on(rec.normal, etai_over_etat)), RGB::new(1.0, 1.0, 1.0)))
+        if etai_over_etat * sin_theta > 1.0 {
+            Some((Ray::new(rec.p, unit_direction.reflect_on(rec.normal)), RGB::new(1.0 , 1.0, 1.0)))
+        } else {
+            Some((Ray::new(rec.p, unit_direction.refract_on(rec.normal, etai_over_etat)), RGB::new(1.0, 1.0, 1.0)))
+        }
     }
 }
