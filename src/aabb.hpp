0cde8817f7b315c335e09b3ef139c49bde85a267
Marcythm <marcythm@gmail.com>
2021-02-09 22:27:05 +0800

    3.5. An Optimized AABB Hit Method

--- src/aabb.hpp
+++ src/aabb.hpp
@@ -5,24 +5,23 @@
 #include "vec3.hpp"
 #include "ray.hpp"
 
-class AABB { // Axis-Aligned Bounding Box
-    p3d _min;
-    p3d _max;
+struct AABB { // Axis-Aligned Bounding Box
+    p3d min;
+    p3d max;
 
 public:
     AABB() = default;
-    AABB(const p3d &a, const p3d &b): _min(a), _max(b) {}
-
-    auto min() -> p3d { return _min; }
-    auto max() -> p3d { return _max; }
+    AABB(const p3d &a, const p3d &b): min(a), max(b) {}
 
     auto hit(const Ray &ray, const f64 t_min, const f64 t_max) const -> bool {
         f64 tmin = t_min, tmax = t_max;
         for (i32 i = 0; i < 3; ++i) {
-            const f64 t0 = (_min[i] - ray.origin()[i]) / ray.direction()[i];
-            const f64 t1 = (_max[i] - ray.origin()[i]) / ray.direction()[i];
-            tmin = std::fmax(tmin, std::fmin(t0, t1));
-            tmax = std::fmin(tmax, std::fmax(t0, t1));
+            const f64 inv_d = 1.0 / ray.direction[i];
+            f64 t0 = (min[i] - ray.origin[i]) * inv_d;
+            f64 t1 = (max[i] - ray.origin[i]) * inv_d;
+            if (inv_d < 0.0) std::swap(t0, t1);
+            tmin = t0 > tmin ? t0 : tmin;
+            tmax = t1 < tmax ? t1 : tmax;
             if (tmax <= tmin)
                 return false;
         }
