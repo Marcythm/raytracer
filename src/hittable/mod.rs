a9fc577c05476e970df19c0198ccdfa679ee22eb
Marcythm <marcythm@gmail.com>
2021-02-10 11:08:28 +0800

    move HittableList into mod.rs

--- src/hittable/mod.rs
+++ src/hittable/mod.rs
@@ -1,11 +1,10 @@
 pub mod sphere;
-pub mod hittablelist;
 
 pub mod prelude {
     pub use super::HitRecord;
     pub use super::Hittable;
+    pub use super::HittableList;
 
-    pub use super::hittablelist::HittableList;
     pub use super::sphere::Sphere;
 }
 
@@ -37,4 +36,38 @@ impl HitRecord {
 
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
+
+#[derive(Clone, Default)]
+pub struct HittableList {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+
+    pub fn push<T: Hittable + 'static>(&mut self, object: T) {
+        self.objects.push(Rc::new(object));
+    }
+    pub fn push_ptr<T: Hittable + 'static>(&mut self, ptr: Rc<T>) {
+        self.objects.push(ptr);
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut sol = None;
+        let mut closest = t_max;
+
+        for object in &self.objects {
+            if let Some(subsol) = object.hit(&ray, t_min, closest) {
+                closest = subsol.t;
+                sol = Some(subsol);
+            }
+        }
+
+        sol
+    }
 }
