d0ac6ca03efdf2bb1a46fde0c45d40e3a36aa041
Marcythm <marcythm@gmail.com>
2021-02-11 02:59:01 +0800

    Creating an Empty “Cornell Box”

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ use hittable::prelude::*;
 use hittable::sphere::Sphere;
 use hittable::moving_sphere::MovingSphere;
 use hittable::aarectangle::XYAARectangle;
-// use hittable::aarectangle::YZAARectangle;
-// use hittable::aarectangle::ZXAARectangle;
+use hittable::aarectangle::YZAARectangle;
+use hittable::aarectangle::ZXAARectangle;
 
 // use material::prelude::*;
 use material::lambertian::Lambertian;
@@ -34,7 +34,8 @@ fn random_scene(rng: &mut SmallRng) -> HittableList {
     let mut hittables = HittableList::default();
 
     let checker = Rc::new(CheckerTexture::with_color(RGB::new(0.2, 0.3, 0.1), RGB::new(0.9, 0.9, 0.9)));
-    hittables.push(Sphere::new(P3d::new(0.0, -1000.0, 0.0), 1000.0, Rc::new(Lambertian::with_texture(checker.clone()))));
+    let checker_gound = Rc::new(Lambertian::with_texture(checker.clone()));
+    hittables.push(Sphere::new(P3d::new(0.0, -1000.0, 0.0), 1000.0, checker_gound.clone()));
 
     // let material_ground = Lambertian::with_color(RGB::new(0.5, 0.5, 0.5));
     // hittables.push(Sphere::new(P3d::new(0.0, -1000.0, 0.0), 1000.0, Rc::new(material_ground)));
@@ -49,32 +50,32 @@ fn random_scene(rng: &mut SmallRng) -> HittableList {
                 if which_material < 0.8 {
                     // diffuse
                     let albedo = RGB::random(0.0, 1.0, rng) * RGB::random(0.0, 1.0, rng);
-                    let sphere_material = Lambertian::with_color(albedo);
+                    let sphere_material = Rc::new(Lambertian::with_color(albedo));
                     let center1 = center + Vec3::new(0.0, rng.gen_range(0.0, 0.5), 0.0);
-                    hittables.push(MovingSphere::new(center, center1, 0.0, 1.0, 0.2, Rc::new(sphere_material)));
+                    hittables.push(MovingSphere::new(center, center1, 0.0, 1.0, 0.2, sphere_material.clone()));
                 } else if which_material < 0.95 {
                     // metal
                     let albedo = RGB::random(0.5, 1.0, rng);
                     let fuzz = rng.gen_range(0.0, 0.5);
-                    let sphere_material = Metal::new(albedo, fuzz);
-                    hittables.push(Sphere::new(center, 0.2, Rc::new(sphere_material)));
+                    let sphere_material = Rc::new(Metal::new(albedo, fuzz));
+                    hittables.push(Sphere::new(center, 0.2, sphere_material.clone()));
                 } else {
                     // glass
-                    let sphere_material = Dielectric::new(1.5);
-                    hittables.push(Sphere::new(center, 0.2, Rc::new(sphere_material)));
+                    let sphere_material = Rc::new(Dielectric::new(1.5));
+                    hittables.push(Sphere::new(center, 0.2, sphere_material.clone()));
                 }
             }
         }
     }
 
-    let material1 = Dielectric::new(1.5);
-    hittables.push(Sphere::new(P3d::new( 0.0, 1.0, 0.0), 1.0, Rc::new(material1)));
+    let material1 = Rc::new(Dielectric::new(1.5));
+    hittables.push(Sphere::new(P3d::new( 0.0, 1.0, 0.0), 1.0, material1.clone()));
 
-    let material2 = Lambertian::with_color(RGB::new(0.4, 0.2, 0.1));
-    hittables.push(Sphere::new(P3d::new(-4.0, 1.0, 0.0), 1.0, Rc::new(material2)));
+    let material2 = Rc::new(Lambertian::with_color(RGB::new(0.4, 0.2, 0.1)));
+    hittables.push(Sphere::new(P3d::new(-4.0, 1.0, 0.0), 1.0, material2.clone()));
 
-    let material3 = Metal::new(RGB::new(0.7, 0.6, 0.5), 0.0);
-    hittables.push(Sphere::new(P3d::new( 4.0, 1.0, 0.0), 1.0, Rc::new(material3)));
+    let material3 = Rc::new(Metal::new(RGB::new(0.7, 0.6, 0.5), 0.0));
+    hittables.push(Sphere::new(P3d::new( 4.0, 1.0, 0.0), 1.0, material3.clone()));
 
     hittables
 }
@@ -118,12 +119,30 @@ fn simple_light(rng: &mut SmallRng) -> HittableList {
     hittables
 }
 
+fn cornell_box() -> HittableList {
+    let mut hittables = HittableList::default();
+
+    let red   = Rc::new(Lambertian::with_rgb(0.65, 0.05, 0.05));
+    let white = Rc::new(Lambertian::with_rgb(0.73, 0.73, 0.73));
+    let green = Rc::new(Lambertian::with_rgb(0.12, 0.45, 0.15));
+    let light = Rc::new(DiffuseLight::with_rgb(15.0, 15.0, 15.0));
+
+    hittables.push(YZAARectangle::new(  0.0, 555.0,   0.0, 555.0, 555.0, green.clone()));
+    hittables.push(YZAARectangle::new(  0.0, 555.0,   0.0, 555.0,   0.0, red.clone()));
+    hittables.push(ZXAARectangle::new(227.0, 332.0, 213.0, 343.0, 554.0, light.clone()));
+    hittables.push(ZXAARectangle::new(  0.0, 555.0,   0.0, 555.0,   0.0, white.clone()));
+    hittables.push(ZXAARectangle::new(  0.0, 555.0,   0.0, 555.0, 555.0, white.clone()));
+    hittables.push(XYAARectangle::new(  0.0, 555.0,   0.0, 555.0, 555.0, white.clone()));
+
+    hittables
+}
+
 fn main() {
     let mut rng = SmallRng::from_entropy();
 
     // Image
-    let aspect_ratio = ASPECT_RATIO;
-    let image_width = IMAGE_WIDTH;
+    let mut aspect_ratio = ASPECT_RATIO;
+    let mut image_width = IMAGE_WIDTH;
     let mut samples_per_pixel = SAMPLES_PER_PIXEL;
     let vertical_field_of_view;
     let background;
@@ -167,19 +186,22 @@ fn main() {
             vertical_field_of_view  = 20.0;
         },
         5 => {
-            scene                   = two_spheres();
-            background              = RGB::new(  0.0, 0.0, 0.0);
-            lookfrom                = P3d::new( 13.0, 2.0, 3.0);
-            lookat                  = P3d::new(  0.0, 0.0, 0.0);
-            vertical_field_of_view  = 20.0;
-        },
-        _ => {
             scene                   = simple_light(&mut rng);
             samples_per_pixel       = 400;
             background              = RGB::new(  0.0, 0.0, 0.0);
             lookfrom                = P3d::new( 26.0, 3.0, 6.0);
             lookat                  = P3d::new(  0.0, 2.0, 0.0);
             vertical_field_of_view  = 20.0;
+        },
+        _ => {
+            scene                   = cornell_box();
+            aspect_ratio            = 1.0;
+            image_width             = 600;
+            samples_per_pixel       = 200;
+            background              = RGB::new(   0.0,   0.0,    0.0);
+            lookfrom                = P3d::new( 278.0, 278.0, -800.0);
+            lookat                  = P3d::new( 278.0, 278.0,    0.0);
+            vertical_field_of_view  = 40.0;
         }
     }
 
