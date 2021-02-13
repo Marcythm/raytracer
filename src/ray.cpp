aa33793e34af5529ea0f8e3a3b25183975636d6e
Marcythm <marcythm@gmail.com>
2021-02-13 22:13:06 +0800

    Light Sampling (hard-coded)

--- src/ray.cpp
+++ src/ray.cpp
@@ -6,7 +6,23 @@ auto Ray::color(const Hittable &world, const RGB &background, const i32 depth) c
         const auto &rec = hit.value();
         const RGB emitted = rec.material->emitted(rec.u, rec.v, rec.p);
         if (const auto &scatter = rec.material->scatter(*this, rec); scatter.has_value()) {
-            const auto &[scattered, attenuation, pdf] = scatter.value();
+            const p3d on_light(random_f64(213.0, 343.0), 554.0, random_f64(227.0, 332.0));
+            Vec3 to_light = on_light - rec.p;
+            const f64 distance_squared = to_light.length2();
+            to_light = to_light.unit();
+
+            if (Vec3::dot(to_light, rec.normal) < 0.0)
+                return emitted;
+
+            const f64 light_area = (343.0 - 213.0) * (332.0 - 227.0);
+            const f64 light_cosine = std::fabs(to_light.y);
+            if (light_cosine < EPS)
+                return emitted;
+
+            auto [scattered, attenuation, pdf] = scatter.value();
+            pdf = distance_squared / (light_cosine * light_area);
+            scattered = Ray(rec.p, to_light, time);
+
             return emitted
                  + attenuation
                  * rec.material->scattering_pdf(*this, rec, scattered)
