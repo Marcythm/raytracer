72bb15d631a7a8887cff2a3ab0ec88c845702743
Marcythm <marcythm@gmail.com>
2021-02-14 01:03:27 +0800

    split decl and impl of trait transform

---
+++ src/transform/transform.cpp
@@ -0,0 +1,53 @@
+#include "transform.hpp"
+
+auto Transform::transform_point(const p3d &p) const -> p3d { return p; }
+auto Transform::inverse_transform_point(const p3d &p) const -> p3d { return p; }
+
+auto Transform::transform_vector(const Vec3 &v) const -> Vec3 { return v; }
+auto Transform::inverse_transform_vector(const Vec3 &v) const -> Vec3 { return v; }
+
+auto Transform::transform_ray(const Ray &ray) const -> Ray {
+    return Ray(transform_point(ray.origin), transform_vector(ray.direction), ray.time);
+}
+auto Transform::inverse_transform_ray(const Ray &ray) const -> Ray {
+    return Ray(inverse_transform_point(ray.origin), inverse_transform_vector(ray.direction), ray.time);
+}
+
+auto Transform::transform_box(const AABB &box) const -> AABB {
+    p3d min( INF,  INF,  INF);
+    p3d max(-INF, -INF, -INF);
+
+    for (i32 i = 0; i < 2; ++i)
+        for (i32 j = 0; j < 2; ++j)
+            for (i32 k = 0; k < 2; ++k) {
+                const p3d transformed_p = transform_point(p3d(box[i].x, box[j].y, box[k].z));
+
+                for (i32 axis = 0; axis < 3; ++axis) {
+                    if (min[axis] > transformed_p[axis])
+                        min[axis] = transformed_p[axis];
+                    if (max[axis] < transformed_p[axis])
+                        max[axis] = transformed_p[axis];
+                }
+            }
+
+    return AABB(min, max);
+}
+auto Transform::inverse_transform_box(const AABB &box) const -> AABB {
+    p3d min( INF,  INF,  INF);
+    p3d max(-INF, -INF, -INF);
+
+    for (i32 i = 0; i < 2; ++i)
+        for (i32 j = 0; j < 2; ++j)
+            for (i32 k = 0; k < 2; ++k) {
+                const p3d invserse_transformed_p = inverse_transform_point(p3d(box[i].x, box[j].y, box[k].z));
+
+                for (i32 axis = 0; axis < 3; ++axis) {
+                    if (min[axis] > invserse_transformed_p[axis])
+                        min[axis] = invserse_transformed_p[axis];
+                    if (max[axis] < invserse_transformed_p[axis])
+                        max[axis] = invserse_transformed_p[axis];
+                }
+            }
+
+    return AABB(min, max);
+}
--- src/transform/transform.hpp
+++ src/transform/transform.hpp
@@ -9,55 +9,15 @@
 struct Transform {
     virtual ~Transform() = default;
 
-    virtual auto transform_point(const p3d &p) const -> p3d { return p; }
-    virtual auto inverse_transform_point(const p3d &p) const -> p3d { return p; }
+    virtual auto transform_point(const p3d &p) const -> p3d;
+    virtual auto inverse_transform_point(const p3d &p) const -> p3d;
 
-    virtual auto transform_vector(const Vec3 &v) const -> Vec3 { return v; }
-    virtual auto inverse_transform_vector(const Vec3 &v) const -> Vec3 { return v; }
+    virtual auto transform_vector(const Vec3 &v) const -> Vec3;
+    virtual auto inverse_transform_vector(const Vec3 &v) const -> Vec3;
 
-    virtual auto transform_ray(const Ray &ray) const -> Ray {
-        return Ray(transform_point(ray.origin), transform_vector(ray.direction), ray.time);
-    }
-    virtual auto inverse_transform_ray(const Ray &ray) const -> Ray {
-        return Ray(inverse_transform_point(ray.origin), inverse_transform_vector(ray.direction), ray.time);
-    }
+    virtual auto transform_ray(const Ray &ray) const -> Ray;
+    virtual auto inverse_transform_ray(const Ray &ray) const -> Ray;
 
-    virtual auto transform_box(const AABB &box) const -> AABB {
-        p3d min( INF,  INF,  INF);
-        p3d max(-INF, -INF, -INF);
-
-        for (i32 i = 0; i < 2; ++i)
-            for (i32 j = 0; j < 2; ++j)
-                for (i32 k = 0; k < 2; ++k) {
-                    const p3d transformed_p = transform_point(p3d(box[i].x, box[j].y, box[k].z));
-
-                    for (i32 axis = 0; axis < 3; ++axis) {
-                        if (min[axis] > transformed_p[axis])
-                            min[axis] = transformed_p[axis];
-                        if (max[axis] < transformed_p[axis])
-                            max[axis] = transformed_p[axis];
-                    }
-                }
-
-        return AABB(min, max);
-    }
-    virtual auto inverse_transform_box(const AABB &box) const -> AABB {
-        p3d min( INF,  INF,  INF);
-        p3d max(-INF, -INF, -INF);
-
-        for (i32 i = 0; i < 2; ++i)
-            for (i32 j = 0; j < 2; ++j)
-                for (i32 k = 0; k < 2; ++k) {
-                    const p3d invserse_transformed_p = inverse_transform_point(p3d(box[i].x, box[j].y, box[k].z));
-
-                    for (i32 axis = 0; axis < 3; ++axis) {
-                        if (min[axis] > invserse_transformed_p[axis])
-                            min[axis] = invserse_transformed_p[axis];
-                        if (max[axis] < invserse_transformed_p[axis])
-                            max[axis] = invserse_transformed_p[axis];
-                    }
-                }
-
-        return AABB(min, max);
-    }
+    virtual auto transform_box(const AABB &box) const -> AABB;
+    virtual auto inverse_transform_box(const AABB &box) const -> AABB;
 };
